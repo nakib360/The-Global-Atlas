eb4608e2e471be7db0865198c966f9a6163409ae
nakib360 <fliptechofficial@gmail.com>
2025-07-11 11:12:58 +0600

    updating flaxibality

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
     }, 500);
   };
   return (
-    <div className="p-10 bg-black ">
+    <div className="p-3 md:p-10 bg-black min-h-screen">
       <label className="input w-full">
         <svg
           className="h-[1em] opacity-50"
@@ -42,7 +42,7 @@ const App = () => {
         />
       </label>
 
-      <section className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 my-5">
+      <section className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 md:gap-5 gap-3 md:my-5 my-3">
         {filtered.length === 0 ? (
           <p className="text-red-600 italic font-bold py-2">
             Can't match country name
@@ -52,18 +52,18 @@ const App = () => {
             <div key={idx} className="card bg-base-100 w-full shadow-sm">
               <figure>
                 <img
-                  className="object-cover md:max-h-40 min-h-60 w-full"
+                  className="object-cover md:max-h-40 md:min-h-60 w-full"
                   src={Country.flags.svg}
                   alt={Country.flags.alt}
                 />
               </figure>
-              <div className="card-body">
+              <div className="card-body p-2.5">
                 <h2 className="card-title">{Country.name.common === "Israel" ? "Fuck Israel" : Country.name.common }</h2>
                 <div className="flex gap-2">
-                  <div className="badge badge-outline badge-accent font-bold">
+                  <div className="badge badge-outline badge-accent font-bold h-full">
                     Capital: {Country.capital}
                   </div>
-                  <div className="badge badge-outline badge-accent font-bold">
+                  <div className="badge badge-outline badge-accent font-bold h-full">
                     Region: {Country.region}
                   </div>
                 </div>
